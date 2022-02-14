93574dfe9a1539eca53f4d0c13315054c94f1c94
Mehmet <83899006+mmcabukbt@users.noreply.github.com>
2022-02-14 15:54:08 +0300

    Delete booklike directory

--- booklike/src/services/axios/index.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export const axiosBase = axios.create({
-	baseURL: "http://localhost:3030"
-})
--- booklike/src/views/NotFound.vue
+++
@@ -1,5 +0,0 @@
-<template>
-	<h1 class="text-8xl font-extrabold text-indigo-600 text-center m-14">404</h1>
-	<h1 class="text-5xl font-extrabold text-indigo-600 text-center m-14">Böyle bir sayfa yok!</h1>
-	<h1 class="text-3xl font-semibold text-center m-14">Üzgünüm. Maalesef yanlış gelmişsiniz...</h1>
-</template>
